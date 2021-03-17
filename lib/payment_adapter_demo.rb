b7d782847c89d4e6f88117f638026e7fe4681f7c
= <=>
2021-03-17 17:38:47 +0530

    das

--- lib/payment_adapter_demo.rb
+++ lib/payment_adapter_demo.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "payment_adapter_demo/version"
+require_relative "payment_adapter_demo/razorpay/razorpay"
 
 module PaymentAdapterDemo
 	class Error < StandardError; end
