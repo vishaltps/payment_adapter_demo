c41d97c1342e809eab4c32e53ee8c16807c1f1ea
= <=>
2021-03-18 16:35:05 +0530

    nasj

--- lib/payment_adapter_demo/razorpay/razorpay.rb
+++ lib/payment_adapter_demo/razorpay/razorpay.rb
@@ -3,6 +3,7 @@ module PaymentAdapterDemo
     attr_accessor :api_key, :secret_key
 
     def initialize(**args)
+      puts "args #{args}"
       @api_key = args[:api_key]
       @secret_key = args[:secret_key]
       puts "api_key  = #{args[:api_key]}"
@@ -12,7 +13,5 @@ module PaymentAdapterDemo
     def print_hello_world
       return "Hello World !! #{@api_key} and #{@secret_key}"
     end
-
-
   end
 end
