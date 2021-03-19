35ace18e2f2006cd0ac340535b66b257ddf1b87e
= <=>
2021-03-19 16:38:05 +0530

    knsag

--- lib/payment_adapter_demo/razorpay/razorpay_api.rb
+++ lib/payment_adapter_demo/razorpay/razorpay_api.rb
@@ -1,16 +1,12 @@
-
-
 module PaymentAdapterDemo::Razorpay
 	class RazorpayApi
 		class << self
 			def create_subscription(credentials, plan, subscriber, trial_period_duration)
-				binding.pry
-				client.create_subscription(plan, subscriber, trial_period_duration)
-			end
-
-			def client
-				@client ||= Client.new(RAZORPAY_CONFIG, credentials)
+				Client.new(RAZORPAY_CONFIG, credentials).create_subscription(plan, subscriber, trial_period_duration)
 			end
 		end
+		# def client
+		# 	@client ||= Client.new(RAZORPAY_CONFIG, credentials)
+		# end
 	end
 end
