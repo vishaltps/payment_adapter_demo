711ebd026be9485133647da99859b152f965c7a1
= <=>
2021-03-19 16:19:52 +0530

    da

--- lib/payment_adapter_demo/razorpay/razorpay_api.rb
+++ lib/payment_adapter_demo/razorpay/razorpay_api.rb
@@ -4,6 +4,7 @@ module PaymentAdapterDemo::Razorpay
 	class RazorpayApi
 		class << self
 			def create_subscription(credentials, plan, subscriber, trial_period_duration)
+				binding.pry
 				client.create_subscription(plan, subscriber, trial_period_duration)
 			end
 
