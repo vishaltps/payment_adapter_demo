a3e43a24cd2329423d51251f3856deccf06d5e7e
= <=>
2021-03-20 09:48:51 +0530

    dsdada

--- lib/payment_adapter_demo.rb
+++ lib/payment_adapter_demo.rb
@@ -5,6 +5,7 @@ require 'httparty'
 require_relative "payment_adapter_demo/version"
 require_relative "payment_adapter_demo/constants"
 require_relative "payment_adapter_demo/razorpay/razorpay_onetime"
+require_relative "payment_adapter_demo/razorpay/razorpay_api"
 require_relative "payment_adapter_demo/razorpay/razorpay_recurring"
 require_relative "payment_adapter_demo/razorpay/client"
 require_relative "payment_adapter_demo/razorpay/api_client"
